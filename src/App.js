030823e4e3a912641a7b1b191f78104c7b050868
Miguel Angel Manzano <miguelangelmanzano@macbook-air-de-miguel.home>
2021-10-11 18:44:21 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,62 +1,121 @@
-import { React, useState } from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Redirect,
-  Switch,
-} from "react-router-dom";
-import Usuarios from "./pages/Usuarios";
-import NuevoDestino from "./pages/NuevoDestino";
-import Navbar from "./components/Navbar";
-import Auth from "./pages/Auth";
+import "./App.css";
+import { useState, useEffect } from "react";
+import AlertasApi from "./Componentes/AlertasApi";
+import Navbar from "./Componentes/Navbar";
+import CreaAlerta from "./Componentes/CreaAlerta";
 
 const App = () => {
-  const [tieneAcceso, setTieneAcceso] = useState(false);
-  const [datos, setDatos] = useState({});
-  const [token, setToken] = useState();
+  const url = process.env.REACT_APP_BACKEND_URL;
 
-  // Traemos desde el componente Auth los datos del usuario enviados desde el servidor mediante esta función prop
-  const gestionarAcceso = (dato) => {
-    setDatos(dato); // datos del usuario: email, password y token
-    setTieneAcceso(true); // La variable que indica que está logueado se pone a true
-    setToken(dato.token); // Por si fuera necesario
-    // localStorage.setItem(
-    //   "datosUsuario",
-    //   JSON.stringify(dato({ userId: dato.userId, token: dato.token }))
-    // );
+  const [alertas, setAlertas] = useState([]);
+
+  // Get para recuperar datos de Alertas -------------------------
+
+  const recuperaDatos = async () => {
+    try {
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json();
+      setAlertas(resultado.alerta);
+      setCopia(resultado.alerta);
+      console.log(resultado.alerta);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    recuperaDatos();
+  }, []);
+
+  //  Eliminar Alerta --------------------------------------------
+
+  const eliminarAlerta = async (id) => {
+    var requestOptions = {
+      method: "DELETE",
+      redirect: "follow",
+    };
+
+    await fetch(`https://git.heroku.com/alerts-ma.git/${id}`, requestOptions)
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
+    recuperaDatos();
   };
 
-  // const gestionarLogout = () => {
-  //   setToken(null);
-  //   setTieneAcceso(false);
-  // };
+  // Crear Alerta --------------------------------------------------
 
-  // useEffect(() => {
-  //   const datosRecuperar = JSON.parse(localStorage.getItem("datosUsuario"));
-  //   if (datosRecuperar && datosRecuperar.token) {
-  //     setToken(datosRecuperar.token);
-  //   }
-  //   console.log(datosRecuperar);
-  // }, []);
+  const creaAlerta = async (alerta) => {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+      Reminder: alerta.reminder,
+      Veces: alerta.veces,
+      Fecha: alerta.fecha,
+      Notas: alerta.notas,
+    });
+
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    await fetch("https://git.heroku.com/alerts-ma.git", requestOptions)
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
+    recuperaDatos();
+  };
+
+  // Modificar Alerta --------------------------------------------
+
+  const modificarAlerta = async (alerta) => {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+      Reminder: alerta.reminder,
+      Veces: alerta.veces,
+      Fecha: alerta.fecha,
+      Notas: alerta.notas,
+    });
+
+    var requestOptions = {
+      method: "PUT",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    await fetch(`https://git.heroku.com/alerts-ma.git/${alerta.id}`, requestOptions)
+      .then((response) => response.text())
+      .catch((error) => console.log("error", error));
+    recuperaDatos();
+  };
+
+  const [copia, setCopia] = useState([]);
+  const añadirElementosEncontrados = (buscador) => {
+    const elementosEncontrados = copia.filter((elemento) => {
+      return elemento.Reminder.toLowerCase().startsWith(buscador.toLowerCase());
+    });
+    setAlertas(elementosEncontrados);
+  };
 
   return (
-    <div className="listado">
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route exact path="/">
-          <Usuarios />
-        </Route>
-        <Route path="/destinos/nuevodestino">
-          <NuevoDestino acceso={tieneAcceso} datos={datos} />
-        </Route>
-        <Route path="/auth">
-          <Auth gestionarAcceso={gestionarAcceso} />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
-    </Router>
+    <div>
+      <Navbar
+        alertas={alertas}
+        onAñadirElementosEncontrados={añadirElementosEncontrados}
+      />
+      <CreaAlerta onCreaAlerta={creaAlerta} />
+      <AlertasApi
+        onAlertas={alertas}
+        onEliminarAlerta={eliminarAlerta}
+        onModificarAlerta={modificarAlerta}
+      />
     </div>
   );
 };
+
 export default App;
